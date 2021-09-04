c87d2edb58af034da0d7da7767773e4dc91a83a4
Steve Calvert <scalvert@linkedin.com>
2021-09-04 12:22:21 -0700

    chore: Adds API to ensure correct cleanup after render

--- __tests__/ink-render-string-test.tsx
+++ __tests__/ink-render-string-test.tsx
@@ -1,12 +1,64 @@
-import * as React from "react";
-import { Text } from "ink";
-import { render } from "../src/index";
+import * as React from 'react';
+import { Text } from 'ink';
+import { render } from '../src/index';
 
-describe("Test ink render string", () => {
-  it("render API can return string", () => {
+describe('ink-render-string', () => {
+  it('render API can return rendered output', () => {
     const Test = () => <Text>Hello World</Text>;
-    const result = render(<Test />);
+    const { output, cleanup } = render(<Test />);
 
-    expect(result).toContain(`Hello World`);
+    expect(output).toEqual('Hello World');
+
+    cleanup();
+  });
+
+  it('returns instance properties', () => {
+    const Test = () => (
+      <>
+        <Text>Hello World</Text>
+        <Text>Goodbye, cruel world!</Text>
+      </>
+    );
+    const instance = render(<Test />);
+
+    expect(instance).toMatchInlineSnapshot(`
+      Object {
+        "cleanup": [Function],
+        "frames": Array [
+          "Hello World
+      Goodbye, cruel world!",
+        ],
+        "output": "Hello World
+      Goodbye, cruel world!",
+        "stderr": OutputStream {
+          "_events": Object {},
+          "_eventsCount": 0,
+          "_maxListeners": undefined,
+          "frames": Array [],
+          "lastFrame": [Function],
+          "write": [Function],
+          Symbol(kCapture): false,
+        },
+        "stdout": OutputStream {
+          "_events": Object {
+            "resize": [Function],
+          },
+          "_eventsCount": 1,
+          "_lastFrame": "Hello World
+      Goodbye, cruel world!",
+          "_maxListeners": undefined,
+          "frames": Array [
+            "Hello World
+      Goodbye, cruel world!",
+          ],
+          "lastFrame": [Function],
+          "write": [Function],
+          Symbol(kCapture): false,
+        },
+        "unmount": [Function],
+      }
+    `);
+
+    instance.cleanup();
   });
 });
